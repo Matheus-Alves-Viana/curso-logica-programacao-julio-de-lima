33a5a7e83153b29051652f8a9199291dd1bf1a8d
Matheus Viana <matheusviana762z@gmail.com>
2025-09-07 01:24:43 -0300

    ajuste na mais

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -6,6 +6,7 @@ describe('Testes da função de soma', function () {
         const resultadoDaSoma = somarDoisNumeros(5, 3);
         expect(resultadoDaSoma).to.equal(8);
     });
+    
 
     it('A função deve ser capaz de somar um numero positivo e um negativo', function (){
         const resultadoDaSoma = somarDoisNumeros(50, -15);
