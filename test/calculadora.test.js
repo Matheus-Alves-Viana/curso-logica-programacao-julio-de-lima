3e0c2d0d15ff9b215a478bc6d510e5b849421821
Matheus Viana <matheusviana762z@gmail.com>
2025-09-07 01:22:49 -0300

    adiconou testes automatizados para funcao calculadora

---
+++ test/calculadora.test.js
@@ -0,0 +1,18 @@
+const { somarDoisNumeros } = require('../src/calculadora');
+const { expect } = require('chai');
+
+describe('Testes da função de soma', function () {
+    it('A funçao deve ser capaz de somar dois numeros positivos', function () {
+        const resultadoDaSoma = somarDoisNumeros(5, 3);
+        expect(resultadoDaSoma).to.equal(8);
+    });
+
+    it('A função deve ser capaz de somar um numero positivo e um negativo', function (){
+        const resultadoDaSoma = somarDoisNumeros(50, -15);
+        expect(resultadoDaSoma).to.equal(35);
+    });
+
+});
+     
+
+
--- tests/calculadora.test.js
+++
@@ -1,5 +0,0 @@
-const { somarDoisNumeros } = require('../src/calculadora');
-
-const resultado = somarDoisNumeros(5, 3);
-
-console.log(resultado)
